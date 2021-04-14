41e93697acfd5af15596dd96e37b649de7ad9e35
Timis <timis.alexioan@gmail.com>
2021-04-14 21:27:28 +0300

    Add Module 1

---
+++ modul1.py
@@ -0,0 +1,29 @@
+# print('Hello World')
+# print('print\u200b("")')
+# print​("")
+HelloWorld = 'Hello World'
+print(HelloWorld)
+
+name = 'Alex'
+# print(100 * (name+"\n"))
+
+print(80*'#')
+print(3 * name)
+print(name * 3)
+
+print(print)
+
+# print int
+# print(3)
+# print('3')
+
+print('Triunghi: ')
+print("   / \ ")
+print("  /   \ ")
+print(' /     \ ')
+print("/_______\ ")
+
+print('Patrat: ')
+print(" _____")
+print("|     |")
+print("|_____|")
