cc769d3dc965815c5c66c4b009d27cbb663ad949
Timiselu <82531368+Timiselu@users.noreply.github.com>
2021-06-14 01:35:36 +0300

    Update H4_App1.py

--- Homework4/H4_App1.py
+++ Homework4/H4_App1.py
@@ -12,29 +12,46 @@
 # argument (x) and calculates ax^2+bx+c
 
 def build(a, b, c):
-    pass  # <your code here>
+    def response(x):
+        expression = a*(x**2)+b*x+c
+        return expression
+    return response(0)
 
+print(build(1, -2, 2))
+print(build(2, -4, 4))
+print(build(3, -6, 6))
 
 # 20P
-# Create a list of response functions by calling build function with the arguments (1,-2,3), (2,-4,4), (3,-6,5)
-list_of_functions = []
-for a, b, c in ((1, -2, 2), (2, -4, 4), (3, -6, 6)):
-    pass  # <your code here>
+# Create a list of response functions by calling build function with the arguments (1,-2,2), (2,-4,4), (3,-6,5)
+
+list = []
+for i, j, k in ((1, -2, 2), (2, -4, 4), (3, -6, 6)):
+    r = build(i, j, k)
+    list.append(r)
+print(list)
 
 # 20P
 # Create a dictionary that contains the result functions as keys and as values the list of results from calling that
 # function with x in range -10, 10 as value
-dict_of_results = {}
-for function in list_of_functions:
-    pass  # <your code here>
+
+results_dict = {}
+for f in list:
+    results_dict[f] = list
+print(results_dict)
 
 # 20P
 # Check dict_of_results and determine which function has the smallest value in the list of values
+
 function_with_smallest_result = None
 smallest_value = None
-for function, values in dict_of_results.items():
-    pass  # <your code here>
+
+for f, values in results_dict.items():
+    if smallest_value is None or smallest_value > values:
+        smallest_value = values
+        function_with_smallest_result = f
+print(smallest_value, function_with_smallest_result)
 
 # 20P
 # Call function_with_smallest_result with argument x = 0 and print the returned value (you should get 2)
-pass  # <your code here>
+res = function_with_smallest_result
+print(res)
