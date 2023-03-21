f1dadbc668bcedf9b5b7d8099aec48d4e4294e8b
Alex Timis <82531368+aitimis@users.noreply.github.com>
2023-03-21 22:33:23 +0200

    Delete Homework2 directory

--- Homework2/H2_App1.py
+++
@@ -1,8 +0,0 @@
-# 20P
-# 1) Prove that "and" operation takes precedence over "or" operation by setting
-# parentheses in the following expression (False or False and True or True)
-
-print("Proving that \"and\" takes precedence over \"or\"")
-print("By printing -> ((False or False) and (True or True))")
-
-print((False or False) and (True or True))
